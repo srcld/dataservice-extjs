0735cb31aa3118cfa51574a603c417ed6e361b68
hardwaresoftwork <760988+hardwaresoftwork@users.noreply.github.com>
2025-01-11 17:08:32 +0100

    use version from npm

--- build.js
+++ build.js
@@ -5,14 +5,15 @@ const moduleSettings = require('./settings/module/moduleSettings'),
     sdk = require('@srcld/bpc-sdk-unofficial'),
     packageJson = require('./package.json');
 
-const {name, applicationName, bpcVersion = '', bpcPropertiesFile = ''} = packageJson.bpcCompat;
+const {name, applicationName} = packageJson.bpcCompat;
+const {version} = packageJson;
 
 const settings = {
     instance: {module: instanceSettings, config},
     module: {module: moduleSettings, config: configModule},
 }
 
-sdk.buildPackage(name, applicationName, settings, bpcVersion)
+sdk.buildPackage(name, applicationName, settings, version)
     .then(() => {
         console.log('Yo.')
     })
